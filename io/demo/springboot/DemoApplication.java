c4fa74973f3b381cfb7c6a20e672f48dba2416bf
Mayavansri <mayasri21072000@gamil.com>
2022-12-19 14:59:36 +0530

    Update version

--- io/demo/springboot/DemoApplication.java
+++ io/demo/springboot/DemoApplication.java
@@ -17,7 +17,7 @@ class DemoApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-		@Bean
+		@Bean 
 		   public Docket productApi() {
 		      return new Docket(DocumentationType.SWAGGER_2).select()
 		         .apis(RequestHandlerSelectors.basePackage("io.demo.springboot")).build();
